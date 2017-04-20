d12f610d7ed64a16e6de614e99d48a41fbe80975
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-20 15:31:50 +0200

    Implements signature validation

--- index.js
+++ index.js
@@ -6,19 +6,33 @@ const { json, send } = require('micro')
 const repackData = require('./lib/repack-data')
 const getPkg = require('./lib/get-pkg')
 const publish = require('./lib/publish')
+const validatePayload = require('./lib/validate-payload')
 
 module.exports = async (request, response) => {
   if (request.method === 'POST') {
-    let data = repackData(await json(request))
-    const pkg = await getPkg(data)
-    data.pkg = pkg
-    const published = await publish(data)
-    const result = {
-      action: published.action !== 'nothing',
-      log: published.log || '',
-      data: data
+    const headers = request.headers
+    const payload = await json(request)
+    const isValid = validatePayload({headers: headers, body: payload})
+
+    if (isValid === true) {
+      try {
+        let data = repackData(payload)
+        const pkg = await getPkg(data)
+        data.pkg = pkg
+        const published = await publish(data)
+        const result = {
+          action: published.action !== 'nothing',
+          log: published.log || '',
+          data: data
+        }
+        send(response, 200, result)
+      } catch (error) {
+        console.error(error)
+        send(response, 500, {success: false, error: error.message})
+      }
+    } else {
+      send(response, 401, 'invalid signature')
     }
-    send(response, 200, result)
   } else {
     const readme = readFileSync('./README.md', 'utf-8')
     const html = marked(readme)
